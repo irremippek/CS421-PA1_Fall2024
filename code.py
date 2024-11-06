375fc50902248269c94f514e4f6163f0d49695b6
irem <96685358+irremippek@users.noreply.github.com>
2024-11-06 19:00:16 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 # CS 421 Programming Assignment 1 - PseudoGit
+import base64
 
 def greet_the_student():
-    return "Hello World"
+    return "22002782"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
